f0a5bbc6d628867fd78757ea70a6688d6c3f355c
Izhar Firdaus <izhar@kagesenshi.org>
2015-07-05 00:41:03 +0800

    fixes for platocdp-base docker

--- docker/platocdp-base/prep.sh
+++ docker/platocdp-base/prep.sh
@@ -5,14 +5,4 @@ git clone https://github.com/koslab/PlatoCDP.git platocdp
 
 cd /opt/platocdp/
 wget http://downloads.buildout.org/2/bootstrap.py -O bootstrap.py
-cat << EOF > eggcache.cfg
-[buildout]
-extends = buildout.cfg
-parts = eggcache
-
-[eggcache]
-recipe = zc.recipe.egg
-eggs = \${instance1:eggs}
-EOF
-
 chown -R plone:plone /opt/platocdp/
